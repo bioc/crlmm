0710eb77cf40d6f028f85532dfd96eedfc5d3d01
Nishant Gopalakrishnan <ngopalak@fhcrc.org>
2009-01-16 00:50:49 +0000

    Added registration for C functions
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/crlmm@36696 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ src/crlmm.h
@@ -0,0 +1,33 @@
+#include <R.h>
+#include <Rinternals.h>
+
+
+SEXP gtypeCallerPart1nm(SEXP *, SEXP *, SEXP *, SEXP *, SEXP *,SEXP *, SEXP *,
+		        SEXP *, SEXP *, SEXP *, SEXP *, SEXP *,SEXP *, SEXP *,
+                        SEXP *, SEXP *, SEXP *);
+
+
+SEXP test (SEXP *);
+
+SEXP gtypeCallerPart1NormalNoN(SEXP *, SEXP *, SEXP *, SEXP *,SEXP *, SEXP *,
+                               SEXP *, SEXP *, SEXP *, SEXP *,SEXP *, SEXP *,
+		               SEXP *, SEXP *, SEXP *, SEXP *,SEXP *);
+
+SEXP gtypeCallerPart2(SEXP *, SEXP *, SEXP *, SEXP *,
+		      SEXP *, SEXP *, SEXP *, SEXP *,
+                      SEXP *, SEXP *, SEXP *, SEXP *,
+		      SEXP *, SEXP *, SEXP *, SEXP *,
+                      SEXP *, SEXP *, SEXP *);
+
+SEXP gtypeCallerPart1TNoN(SEXP *, SEXP *, SEXP *, SEXP *,
+		          SEXP *, SEXP *, SEXP *, SEXP *,
+                          SEXP *, SEXP *, SEXP *, SEXP *,
+		          SEXP *, SEXP *, SEXP *,
+		      SEXP *, SEXP *)
+
+
+SEXP gtypeCallerPart2TNoN(SEXP *, SEXP *, SEXP *, SEXP *,
+			  SEXP *, SEXP *, SEXP *, SEXP *, 
+			  SEXP *, SEXP *, SEXP *, SEXP *,
+			  SEXP *, SEXP *, SEXP *, SEXP *,
+			  SEXP *, SEXP *, SEXP *)
---
+++ src/init.c
@@ -0,0 +1,23 @@
+#include <R.h>
+#include <R_ext/Rdynload.h>
+#include "crlmm.h"
+
+static const R_CallMethodDef CallEntries[] = {
+    {"test", (DL_FUNC)&test, 1},
+    {"gtypeCallerPart1nm", (DL_FUNC)&gtypeCallerPart1nm, 17},
+    {"gtypeCallerPart2nm", (DL_FUNC)&gtypeCallerPart2nm, 19},
+    {"gtypeCallerPart1NormalNoN", (DL_FUNC)&gtypeCallerPart1NormalNoN, 17},
+    {"gtypeCallerPart1", (DL_FUNC)&gtypeCallerPart1, 17},
+    {"gtypeCallerPart2", (DL_FUNC)&gtypeCallerPart2, 19},
+    {"gtypeCallerPart1TNoN", (DL_FUNC)&gtypeCallerPart1TNoN, 17},
+    {"gtypeCallerPart2TNoN", (DL_FUNC)&gtypeCallerPart2TNoN, 19},
+    {NULL, NULL, 0}
+};
+
+void R_init_flowCore(DllInfo *dll)
+{
+    R_registerRoutines(dll, NULL, CallEntries, NULL, NULL);    
+}
+
+
+
