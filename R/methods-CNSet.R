bf244505db0a362010310151395c084653c9384b
Rob Scharp <rscharpf@jhsph.edu>
2010-08-21 02:47:41 +0000

    v1.7.11  removed 'copyNumber' method from methods-CNSet.R.
    Commented CA, CB initialization in the coercion of SnpSuperSet to CNSetLM.
    
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/crlmm@48928 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/methods-CNSet.R
+++ R/methods-CNSet.R
@@ -50,8 +50,8 @@ setAs("SnpSuperSet", "CNSetLM", function(from, to){
 		     alleleB=B(from),
 		     call=snpCall(from),
 		     callProbability=snpCallProbability(from),
-		     CA=initializeBigMatrix("CA", nrow(from), ncol(from)),
-		     CB=initializeBigMatrix("CB", nrow(from), ncol(from)),
+##		     CA=initializeBigMatrix("CA", nrow(from), ncol(from)),
+##		     CB=initializeBigMatrix("CB", nrow(from), ncol(from)),
 		     annotation=annotation(from),
 		     featureData=featureData(from),
 		     experimentData=experimentData(from),
@@ -105,24 +105,6 @@ setMethod("computeCopynumber", "CNSet",
 	object
 })
 
-setMethod("copyNumber", "CNSet", function(object){
-	I <- isSnp(object)
-	ffIsLoaded <- class(calls(object))[[1]]=="ff"
-	CA <- CA(object)
-	CB <- CB(object)
-	if(ffIsLoaded){
-		open(CA)
-		open(CB)
-		CA <- as.matrix(CA[,])
-		CB <- as.matrix(CB[,])
-	}
-	CN <- CA + CB
-	##For nonpolymorphic probes, CA is the total copy number
-	CN[!I, ] <- CA(object)[!I, ]
-	CN <- CN/100
-	CN
-})
-
 setMethod("totalCopyNumber", "CNSet", function(object, i, j){
 	if(missing(i) & missing(j)){
 		if(inherits(CA(object), "ff") | inherits(CA(object), "ffdf")) stop("Must specify i and/or j for ff objects")
@@ -150,57 +132,9 @@ setMethod("totalCopyNumber", "CNSet", function(object, i, j){
 	return(cn.total)
 })
 
-##setMethod("copyNumber", "CNSet", function(object){
-##	I <- isSnp(object)
-##	CA <- CA(object)
-##	CB <- CB(object)
-##	CN <- CA + CB
-##	##For nonpolymorphic probes, CA is the total copy number
-##	CN[!I, ] <- CA(object)[!I, ]
-##	CN
-##})
-
-
 setMethod("ellipse", "CNSet", function(x, copynumber, batch, ...){
 	ellipse.CNSet(x, copynumber, batch, ...)
 })
-
-##setMethod("ellipse", "CNSet", function(x, copynumber, ...){
-ellipse.CNSet <- function(x, copynumber, batch, ...){
-	if(nrow(x) > 1) stop("only 1 snp at a time")
-	##batch <- unique(x$batch)
-	if(missing(batch)){
-		stop("must specify batch")
-	}
-	if(length(batch) > 1) stop("batch variable not unique")
-	nuA <- getParam(x, "nuA", batch)
-	nuB <- getParam(x, "nuB", batch)
-	phiA <- getParam(x, "phiA", batch)
-	phiB <- getParam(x, "phiB", batch)
-	tau2A <- getParam(x, "tau2A", batch)
-	tau2B <- getParam(x, "tau2B", batch)
-	sig2A <- getParam(x, "sig2A", batch)
-	sig2B <- getParam(x, "sig2B", batch)
-	corrA.BB <- getParam(x, "corrA.BB", batch)
-	corrB.AA <- getParam(x, "corrB.AA", batch)
-	corr <- getParam(x, "corr", batch)
-	for(CN in copynumber){
-		for(CA in 0:CN){
-			CB <- CN-CA
-			A.scale <- sqrt(tau2A*(CA==0) + sig2A*(CA > 0))
-			B.scale <- sqrt(tau2B*(CB==0) + sig2B*(CB > 0))
-			scale <- c(A.scale, B.scale)
-			if(CA == 0 & CB > 0) rho <- corrA.BB
-			if(CA > 0 & CB == 0) rho <- corrB.AA
-			if(CA > 0 & CB > 0) rho <- corr
-			if(CA == 0 & CB == 0) rho <- 0
-			lines(ellipse(x=rho, centre=c(log2(nuA+CA*phiA),
-					     log2(nuB+CB*phiB)),
-				      scale=scale), ...)
-		}
-	}
-}
-
 
 setMethod("nu", c("CNSetLM", "character"), function(object, allele){
 	getValue <- function(allele){
