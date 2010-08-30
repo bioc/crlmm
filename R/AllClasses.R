97e1fa718a65e110188b22106533f8383e5265e0
Rob Scharp <rscharpf@jhsph.edu>
2010-08-30 19:41:01 +0000

    Removed setValidity method for CNSetLM
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/crlmm@49139 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/AllClasses.R
+++ R/AllClasses.R
@@ -3,20 +3,3 @@ setOldClass("ff_matrix")
 setOldClass("ffdf")
 ##setClassUnion("ff_or_matrix", c("ff_matrix", "matrix", "ffdf"))
 setClassUnion("ff_or_matrix", c("ff_matrix", "matrix"))
-setClass("CNSetLM", contains="CNSet", representation(lM="list_or_ffdf"))
-setMethod("initialize", "CNSetLM", function(.Object, lM=new("list"), ...){
-	.Object@lM <- lM
-	.Object <- callNextMethod(.Object, ...)
-})
-setValidity("CNSetLM",
-	    function(object){
-		    if(!"batch" %in% varLabels(protocolData(object)))
-			    return("'batch' not defined in protocolData")
-		    if(!"chromosome" %in% fvarLabels(object))
-			    return("'chromosome' not defined in featureData")
-		    if(!"position" %in% fvarLabels(object))
-			    return("'position' not defined in featureData")
-		    if(!"isSnp" %in% fvarLabels(object))
-			    return("'isSnp' not defined in featureData")
-		    return(TRUE)
-	    })
