568168377a4d9b64682462eed7244bb61ec344f1
Rob Scharp <rscharpf@jhsph.edu>
2010-08-30 15:45:15 +0000

    Changes to support humanomniexpress12v1b illumina platform
    - added platform to validCdfNames()
    - added platform to chipList in RGtoXY and RGtoXY2
    
    Other minor changes to copynumber vignette
          - removed eol whitespace
          - removed some commented lines towards the end
          - added a message to indicate loading a saved object
    
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/branches/RELEASE_2_6/madman/Rpacks/crlmm@49102 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/utils.R
+++ R/utils.R
@@ -165,8 +165,10 @@ validCdfNames <- function(){
 	  "human610quadv1b",
 	  "human660quadv1a",
 	  "human1mduov3b",
-	  "humanomni1quadv1b")
+	  "humanomni1quadv1b",
+          "humanomniexpress12v1b")
 }
+
 isValidCdfName <- function(cdfName){
 	chipList <- validCdfNames()
 	result <- cdfName %in% chipList	
