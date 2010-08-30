56b783ecbf71ef95cf4959c85621f72e2bd75ac3
Rob Scharp <rscharpf@jhsph.edu>
2010-08-30 19:41:03 +0000

    Revised accesors in methods-AssayData -- some of the conflicts resolved by hand during rebase were incorrect
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/crlmm@49140 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/methods-AssayData.R
+++ R/methods-AssayData.R
@@ -24,30 +24,6 @@ setMethod("Ns", signature(object="AssayData"),
 		  res <- array(NA, dim=c(nrow(N.AA), 3, ncol(N.AA)))
 		  dimnames(res)[[2]] <- c("AA", "AB", "BB")
 		  dimnames(res)[[3]] <- batchnames
-		  if(missing(i) & missing(j)){
-			  N.AA <- as.matrix(assayDataElement(object, "N.AA"))
-			  N.AB <- as.matrix(assayDataElement(object, "N.AB"))
-			  N.BB <- as.matrix(assayDataElement(object, "N.BB"))
-		  }
-		  if(missing(i) & !missing(j)){
-			  J <- match(batchnames, batchNames(object))
-			  stopifnot(length(J) > 0 & !all(is.na(J)))
-			  N.AA <- as.matrix(assayDataElement(object, "N.AA"))[, J, ...]
-			  N.AB <- as.matrix(assayDataElement(object, "N.AB"))[, J, ...]
-			  N.BB <- as.matrix(assayDataElement(object, "N.BB"))[, J, ...]
-		  }
-		  if(!missing(i) & !missing(j)){
-			  J <- match(batchnames, batchNames(object))
-			  stopifnot(length(J) > 0 & !all(is.na(J)))
-			  N.AA <- as.matrix(assayDataElement(object, "N.AA"))[i, J, ...]
-			  N.AB <- as.matrix(assayDataElement(object, "N.AB"))[i, J, ...]
-			  N.BB <- as.matrix(assayDataElement(object, "N.BB"))[i, J, ...]
-		  }
-		  if(!missing(i) & missing(j)){
-			  N.AA <- as.matrix(assayDataElement(object, "N.AA"))[i, , ...]
-			  N.AB <- as.matrix(assayDataElement(object, "N.AB"))[i, , ...]
-			  N.BB <- as.matrix(assayDataElement(object, "N.BB"))[i, , ...]
-		  }
 		  res[, "AA", ] <- N.AA
 		  res[, "AB", ] <- N.AB
 		  res[, "BB", ] <- N.BB
@@ -79,30 +55,6 @@ setMethod("corr", signature(object="AssayData"),
 		  res <- array(NA, dim=c(nrow(corrAA), 3, ncol(corrAA)))
 		  dimnames(res)[[2]] <- c("AA", "AB", "BB")
 		  dimnames(res)[[3]] <- batchnames
-		  if(missing(i) & missing(j)){
-			  corrAA <- as.matrix(assayDataElement(object, "corrAA"))
-			  corrAB <- as.matrix(assayDataElement(object, "corrAB"))
-			  corrBB <- as.matrix(assayDataElement(object, "corrBB"))
-		  }
-		  if(missing(i) & !missing(j)){
-			  J <- match(batchnames, batchNames(object))
-			  stopifnot(length(J) > 0 & !all(is.na(J)))
-			  corrAA <- as.matrix(assayDataElement(object, "corrAA"))[, J, ...]
-			  corrAB <- as.matrix(assayDataElement(object, "corrAB"))[, J, ...]
-			  corrBB <- as.matrix(assayDataElement(object, "corrBB"))[, J, ...]
-		  }
-		  if(!missing(i) & !missing(j)){
-			  J <- match(batchnames, batchNames(object))
-			  stopifnot(length(J) > 0 & !all(is.na(J)))
-			  corrAA <- as.matrix(assayDataElement(object, "corrAA"))[i, J, ...]
-			  corrAB <- as.matrix(assayDataElement(object, "corrAB"))[i, J, ...]
-			  corrBB <- as.matrix(assayDataElement(object, "corrBB"))[i, J, ...]
-		  }
-		  if(!missing(i) & missing(j)){
-			  corrAA <- as.matrix(assayDataElement(object, "corrAA"))[i, , ...]
-			  corrAB <- as.matrix(assayDataElement(object, "corrAB"))[i, , ...]
-			  corrBB <- as.matrix(assayDataElement(object, "corrBB"))[i, , ...]
-		  }
 		  res[, "AA", ] <- corrAA
 		  res[, "AB", ] <- corrAB
 		  res[, "BB", ] <- corrBB
@@ -128,43 +80,6 @@ setMethod("medians", signature(object="AssayData"),
 		  dimnames(res)[[2]] <- c("A", "B")
 		  dimnames(res)[[3]] <- c("AA", "AB", "BB")
 		  dimnames(res)[[4]] <- batchnames
-		  if(missing(i) & missing(j)){
-			  medianA.AA <- as.matrix(assayDataElement(object, "medianA.AA"))
-			  medianA.AB <- as.matrix(assayDataElement(object, "medianA.AB"))
-			  medianA.BB <- as.matrix(assayDataElement(object, "medianA.BB"))
-			  medianB.AA <- as.matrix(assayDataElement(object, "medianB.AA"))
-			  medianB.AB <- as.matrix(assayDataElement(object, "medianB.AB"))
-			  medianB.BB <- as.matrix(assayDataElement(object, "medianB.BB"))
-		  }
-		  if(missing(i) & !missing(j)){
-			  J <- match(batchnames, batchNames(object))
-			  stopifnot(length(J) > 0 & !all(is.na(J)))
-			  medianA.AA <- as.matrix(assayDataElement(object, "medianA.AA"))[, J, ...]
-			  medianA.AB <- as.matrix(assayDataElement(object, "medianA.AB"))[, J, ...]
-			  medianA.BB <- as.matrix(assayDataElement(object, "medianA.BB"))[, J, ...]
-			  medianB.AA <- as.matrix(assayDataElement(object, "medianB.AA"))[, J, ...]
-			  medianB.AB <- as.matrix(assayDataElement(object, "medianB.AB"))[, J, ...]
-			  medianB.BB <- as.matrix(assayDataElement(object, "medianB.BB"))[, J, ...]
-
-		  }
-		  if(!missing(i) & !missing(j)){
-			  J <- match(batchnames, batchNames(object))
-			  stopifnot(length(J) > 0 & !all(is.na(J)))
-			  medianA.AA <- as.matrix(assayDataElement(object, "medianA.AA"))[i, J, ...]
-			  medianA.AB <- as.matrix(assayDataElement(object, "medianA.AB"))[i, J, ...]
-			  medianA.BB <- as.matrix(assayDataElement(object, "medianA.BB"))[i, J, ...]
-			  medianB.AA <- as.matrix(assayDataElement(object, "medianB.AA"))[i, J, ...]
-			  medianB.AB <- as.matrix(assayDataElement(object, "medianB.AB"))[i, J, ...]
-			  medianB.BB <- as.matrix(assayDataElement(object, "medianB.BB"))[i, J, ...]
-		  }
-		  if(!missing(i) & missing(j)){
-			  medianA.AA <- as.matrix(assayDataElement(object, "medianA.AA"))[i, ...]
-			  medianA.AB <- as.matrix(assayDataElement(object, "medianA.AB"))[i, ...]
-			  medianA.BB <- as.matrix(assayDataElement(object, "medianA.BB"))[i, ...]
-			  medianB.AA <- as.matrix(assayDataElement(object, "medianB.AA"))[i, ...]
-			  medianB.AB <- as.matrix(assayDataElement(object, "medianB.AB"))[i, ...]
-			  medianB.BB <- as.matrix(assayDataElement(object, "medianB.BB"))[i, ...]
-		  }
 		  res[, "A", "AA", ] <- medianA.AA
 		  res[, "A", "AB", ] <- medianA.AB
 		  res[, "A", "BB", ] <- medianA.BB
@@ -193,43 +108,6 @@ setMethod("mads", signature(object="AssayData"),
 		  dimnames(res)[[2]] <- c("A", "B")
 		  dimnames(res)[[3]] <- c("AA", "AB", "BB")
 		  dimnames(res)[[4]] <- batchnames
-		  if(missing(i) & missing(j)){
-			  madA.AA <- as.matrix(assayDataElement(object, "madA.AA"))
-			  madA.AB <- as.matrix(assayDataElement(object, "madA.AB"))
-			  madA.BB <- as.matrix(assayDataElement(object, "madA.BB"))
-			  madB.AA <- as.matrix(assayDataElement(object, "madB.AA"))
-			  madB.AB <- as.matrix(assayDataElement(object, "madB.AB"))
-			  madB.BB <- as.matrix(assayDataElement(object, "madB.BB"))
-		  }
-		  if(missing(i) & !missing(j)){
-			  J <- match(batchnames, batchNames(object))
-			  stopifnot(length(J) > 0 & !all(is.na(J)))
-			  madA.AA <- as.matrix(assayDataElement(object, "madA.AA"))[, J, ...]
-			  madA.AB <- as.matrix(assayDataElement(object, "madA.AB"))[, J, ...]
-			  madA.BB <- as.matrix(assayDataElement(object, "madA.BB"))[, J, ...]
-			  madB.AA <- as.matrix(assayDataElement(object, "madB.AA"))[, J, ...]
-			  madB.AB <- as.matrix(assayDataElement(object, "madB.AB"))[, J, ...]
-			  madB.BB <- as.matrix(assayDataElement(object, "madB.BB"))[, J, ...]
-
-		  }
-		  if(!missing(i) & !missing(j)){
-			  J <- match(batchnames, batchNames(object))
-			  stopifnot(length(J) > 0 & !all(is.na(J)))
-			  madA.AA <- as.matrix(assayDataElement(object, "madA.AA"))[i, J, ...]
-			  madA.AB <- as.matrix(assayDataElement(object, "madA.AB"))[i, J, ...]
-			  madA.BB <- as.matrix(assayDataElement(object, "madA.BB"))[i, J, ...]
-			  madB.AA <- as.matrix(assayDataElement(object, "madB.AA"))[i, J, ...]
-			  madB.AB <- as.matrix(assayDataElement(object, "madB.AB"))[i, J, ...]
-			  madB.BB <- as.matrix(assayDataElement(object, "madB.BB"))[i, J, ...]
-		  }
-		  if(!missing(i) & missing(j)){
-			  madA.AA <- as.matrix(assayDataElement(object, "madA.AA"))[i, ...]
-			  madA.AB <- as.matrix(assayDataElement(object, "madA.AB"))[i, ...]
-			  madA.BB <- as.matrix(assayDataElement(object, "madA.BB"))[i, ...]
-			  madB.AA <- as.matrix(assayDataElement(object, "madB.AA"))[i, ...]
-			  madB.AB <- as.matrix(assayDataElement(object, "madB.AB"))[i, ...]
-			  madB.BB <- as.matrix(assayDataElement(object, "madB.BB"))[i, ...]
-		  }
 		  res[, "A", "AA", ] <- madA.AA
 		  res[, "A", "AB", ] <- madA.AB
 		  res[, "A", "BB", ] <- madA.BB
