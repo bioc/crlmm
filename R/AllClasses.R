7799caf65b3b9c980517c48b156ad3a4b233df66
Rob Scharp <rscharpf@jhsph.edu>
2010-08-30 19:41:12 +0000

    importing ff_matrix class from oligoClasses
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/crlmm@49144 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/AllClasses.R
+++ R/AllClasses.R
@@ -1,5 +1,3 @@
 setOldClass("ellipse")
-setOldClass("ff_matrix")
 setOldClass("ffdf")
-##setClassUnion("ff_or_matrix", c("ff_matrix", "matrix", "ffdf"))
 setClassUnion("ff_or_matrix", c("ff_matrix", "matrix"))
