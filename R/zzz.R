f8f2b803bf4ee1c4896c7a9d00fd1cea6ce4e916
Rob Scharp <rscharpf@jhsph.edu>
2011-10-01 04:48:50 +0000

    Update zzz.R to use packageStartupMessage. Bug fix in copynumberAccessors Rd file
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/crlmm@58648 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/zzz.R
+++ R/zzz.R
@@ -1,11 +1,14 @@
 # Loading required libraries
 THISPKG <- "crlmm"
-.onLoad <- function(libname, pkgname) {
-	require("methods")
-}
+
+##.onLoad <- function(libname, pkgname) {
+##	require("methods")
+##}
+
 .onAttach <- function(libname, pkgname) {
-  message("Welcome to crlmm version ", packageDescription(THISPKG, field="Version"))
+	packageStartupMessage("Welcome to crlmm version ", packageDescription(THISPKG, field="Version"))
 }
+
 .onUnload <- function( libpath ){
 	library.dynam.unload(THISPKG, libpath)
 }
